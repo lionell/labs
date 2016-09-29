b543d72c7d979e55e16beb2c8171f6aa5de0e5f8
Ruslan Sakevych <xlionell@gmail.com>
2016-09-29 19:33:55 +0300

    Add 2 numerical methods labs, and update lab1 from system programming

--- volohov/java/io/github/lionell/lab1/Runner.java
+++ volohov/java/io/github/lionell/lab1/Runner.java
@@ -2,6 +2,10 @@ package io.github.lionell.lab1;
 
 import com.google.common.base.Joiner;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -11,12 +15,14 @@ import io.github.lionell.lab1.io.WordReader;
 /**
  * Usage example: $ bazel run //java/io/github/lionell/lab1 --
  * /home/lionell/dev/labs/volohov/res/in.txt
+ * /home/lionell/dev/labs/volohov/res/out.txt
  *
  * @author lionell
  */
 public class Runner {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     WordReader reader = new WordReader(args[0]);
+    System.out.println(args[0]);
 
     List<String> words = new ArrayList<>();
     while (reader.hasNext()) {
